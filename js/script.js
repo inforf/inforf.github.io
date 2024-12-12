d5c2e5cb0eee2bd7c446d73ad581ab1e7b695641
inforf <30818563+inforf@users.noreply.github.com>
2024-12-12 10:11:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,16 @@
-function showChristmasAlert() {
-    
-    var lastShownDate = localStorage.getItem('lastShownDate');
-    var currentDate = new Date().toISOString().split('T')[0]; 
-
-  
-    if (lastShownDate !== currentDate) {
-        
-        var script = document.createElement("script");
-        script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
-        script.onload = function () {
-           
-            Swal.fire({
-                title: "🎄 Feliz Natal! 🎅",
-                text: "Que seu Natal seja repleto de alegria, amor e paz!",
-                imageUrl: "https://png.pngtree.com/thumb_back/fh260/background/20241025/pngtree-merry-christmas-2024-image_16452006.jpg",
-                imageWidth: 300,
-                imageHeight: 400,
-                background: "#ffe7d1",
-                color: "#d10000",
-                confirmButtonText: "🎁 Obrigado!",
-                footer: "<a href='https://pt.wikipedia.org/wiki/Natal' style='color:#d10000'>Saiba mais sobre o Natal</a>"
-            });
-        };
-        document.head.appendChild(script);
-
-       
-        localStorage.setItem('lastShownDate', currentDate);
-    }
-}
-
-
-showChristmasAlert();
+var script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
+script.onload = function () {
+   
+    Swal.fire({
+        title: "🎄 Feliz Natal! 🎅",
+        text: "Que seu Natal seja repleto de alegria, amor e paz!",
+        imageUrl: "https://png.pngtree.com/thumb_back/fh260/background/20241025/pngtree-merry-christmas-2024-image_16452006.jpg",
+        imageWidth: 300,
+        imageHeight: 400,
+        background: "#ffe7d1",
+        color: "#d10000",
+        confirmButtonText: "🎁 Obrigado!",
+        footer: "<a href='https://pt.wikipedia.org/wiki/Natal' style='color:#d10000'>Saiba mais sobre o Natal</a>"
+    });
+};
