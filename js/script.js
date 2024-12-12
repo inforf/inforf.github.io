5248862a0383544df5753c23b84d30bff64d597e
inforf <30818563+inforf@users.noreply.github.com>
2024-12-12 11:57:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.onload = function() {
     script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
     script.onload = function () {
         var button = document.createElement('button');
-        button.innerText = 'Clique aqui';
+        button.innerText = 'Baos festas!';
         button.classList.add('pedidoOperador', 'btn', 'btn-success', 'js-shortcut-blank');
         button.style.fontSize = '16px';
         button.style.padding = '10px 20px';
@@ -16,24 +16,24 @@ window.onload = function() {
         button.onclick = function() {
             Swal.fire({
                 title: "🎄 Feliz Natal e um Próspero Ano Novo! 🎆",
-                text: "Desejo que este Natal seja repleto de amor, alegria e união, e que o ano novo traga novas conquistas e realizações para você e sua família!",
+                text: "Desejamos que este Natal seja repleto de amor, alegria e união, e que o ano novo traga novas conquistas e realizações para você e sua família!",
                 imageUrl: "https://png.pngtree.com/thumb_back/fh260/background/20241025/pngtree-merry-christmas-2024-image_16452006.jpg",
                 imageWidth: 300,
                 imageHeight: 400,
                 background: "#ffe7d1",
                 color: "#d10000",
-                confirmButtonText: "🎁 Obrigado!",
-                footer: "<a href='https://pt.wikipedia.org/wiki/Natal' style='color:#d10000'>Saiba mais sobre o Natal</a>"
+                confirmButtonText: "🎁 Obrigado!"
+                
             });
         };
 
         var buttonContainer = document.createElement('div');
         buttonContainer.style.display = 'flex';
-        buttonContainer.style.justifyContent = 'center';
-        buttonContainer.style.alignItems = 'center';
+        //buttonContainer.style.justifyContent = 'center';
+       // buttonContainer.style.alignItems = 'center';
         buttonContainer.style.height = '100%';
         buttonContainer.style.width = '100%';
-        buttonContainer.style.marginTop = '20px';
+        //buttonContainer.style.marginTop = '20px';
 
         buttonContainer.appendChild(button);
 
@@ -43,7 +43,7 @@ window.onload = function() {
             if (targetElement) {
                 targetElement.style.display = 'flex';
                 targetElement.style.justifyContent = 'flex-end';
-                targetElement.style.alignItems = 'center';  
+                //targetElement.style.alignItems = 'center';  
                 targetElement.style.position = 'relative';
 
                 targetElement.appendChild(buttonContainer);
