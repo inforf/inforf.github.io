5aa20228d4c1f2b2b26c223a569cd55a82a9b17c
inforf <30818563+inforf@users.noreply.github.com>
2024-12-12 11:04:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
-var script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
-script.onload = function () {
-    var button = document.createElement('button');
-    button.innerText = 'Clique para Feliz Natal!';
-    button.style.fontSize = '16px';
-    button.style.padding = '10px 20px';
-    button.style.backgroundColor = '#ffe7d1';
-    button.style.color = '#d10000';
-    button.style.border = 'none';
-    button.style.cursor = 'pointer';
-    button.style.float = 'right'; 
+window.onload = function() {
+    var script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
+    script.onload = function () {
+        var button = document.createElement('button');
+        button.innerText = 'Clique para Feliz Natal!';
+        button.style.fontSize = '16px';
+        button.style.padding = '10px 20px';
+        button.style.backgroundColor = '#ffe7d1';
+        button.style.color = '#d10000';
+        button.style.border = 'none';
+        button.style.cursor = 'pointer';
+        button.style.float = 'right'; 
 
-    button.onclick = function() {
-        Swal.fire({
-            title: "🎄 Feliz Natal! 🎅",
-            text: "Que seu Natal seja repleto de alegria, amor e paz!",
-            imageUrl: "https://png.pngtree.com/thumb_back/fh260/background/20241025/pngtree-merry-christmas-2024-image_16452006.jpg",
-            imageWidth: 300,
-            imageHeight: 400,
-            background: "#ffe7d1",
-            color: "#d10000",
-            confirmButtonText: "🎁 Obrigado!",
-            footer: "<a href='https://pt.wikipedia.org/wiki/Natal' style='color:#d10000'>Saiba mais sobre o Natal</a>"
-        });
+        button.onclick = function() {
+            Swal.fire({
+                title: "🎄 Feliz Natal! 🎅",
+                text: "Que seu Natal seja repleto de alegria, amor e paz!",
+                imageUrl: "https://png.pngtree.com/thumb_back/fh260/background/20241025/pngtree-merry-christmas-2024-image_16452006.jpg",
+                imageWidth: 300,
+                imageHeight: 400,
+                background: "#ffe7d1",
+                color: "#d10000",
+                confirmButtonText: "🎁 Obrigado!",
+                footer: "<a href='https://pt.wikipedia.org/wiki/Natal' style='color:#d10000'>Saiba mais sobre o Natal</a>"
+            });
+        };
+
+        var checkElementExist = setInterval(function() {
+            var targetElement = document.querySelector('div.row:nth-child(2)');
+            
+            if (targetElement) {
+                targetElement.appendChild(button);
+                clearInterval(checkElementExist); 
+            }
+        }, 100); 
     };
-
-    var checkElementExist = setInterval(function() {
-        var targetElement = document.querySelector('div.row:nth-child(2)');
-        
-        if (targetElement) {
-            targetElement.appendChild(button);
-            clearInterval(checkElementExist);  
-        }
-    }, 100); 
+    document.head.appendChild(script);
 };
-
-document.head.appendChild(script);
