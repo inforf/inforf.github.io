a0fa063805ea105bad9b8d189f0dc68f000a5adb
inforf <30818563+inforf@users.noreply.github.com>
2024-12-12 11:49:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,17 @@ window.onload = function() {
     var script = document.createElement("script");
     script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
     script.onload = function () {
-        // Criação do botão
         var button = document.createElement('button');
         button.innerText = 'Clique aqui';
         button.classList.add('pedidoOperador', 'btn', 'btn-success', 'js-shortcut-blank');
         button.style.fontSize = '16px';
         button.style.padding = '10px 20px';
-        button.style.marginTop = '10px'; 
+        button.style.marginTop = '10px';
         button.style.position = 'relative';
         button.style.width = 'auto';
-        button.style.display = 'inline-block'; 
+        button.style.display = 'inline-block';
+        button.style.marginBottom = '20px';
 
-        // Definir a ação do botão
         button.onclick = function() {
             Swal.fire({
                 title: "🎄 Feliz Natal! 🎅",
@@ -28,29 +27,25 @@ window.onload = function() {
             });
         };
 
-       
         var buttonContainer = document.createElement('div');
         buttonContainer.style.display = 'flex';
-        buttonContainer.style.justifyContent = 'center';  
-        buttonContainer.style.alignItems = 'center';  
-        buttonContainer.style.height = '100%';  
-        buttonContainer.style.width = '100%';  
+        buttonContainer.style.justifyContent = 'center';
+        buttonContainer.style.alignItems = 'center';
+        buttonContainer.style.height = '100%';
+        buttonContainer.style.width = '100%';
+        buttonContainer.style.marginTop = '20px';
 
-        
         buttonContainer.appendChild(button);
 
-        
         var checkElementExist = setInterval(function() {
             var targetElement = document.querySelector('div.row:nth-child(2)');
             
             if (targetElement) {
-                
                 targetElement.style.display = 'flex';
                 targetElement.style.justifyContent = 'flex-end';
                 targetElement.style.alignItems = 'center';  
                 targetElement.style.position = 'relative';
 
-                
                 targetElement.appendChild(buttonContainer);
                 clearInterval(checkElementExist);
             }
