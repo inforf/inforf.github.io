ab6a11e65ef89dc23263385cb8bdad4df49b7f7d
inforf <30818563+inforf@users.noreply.github.com>
2024-12-12 12:56:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.onload = function() {
     script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
     script.onload = function () {
         var button = document.createElement('button');
-        button.innerText = 'Clique aqui';
+        button.innerText = 'Boas Festas!';
         button.classList.add('pedidoOperador', 'btn', 'btn-success', 'js-shortcut-blank');
         button.style.fontSize = '16px';
         button.style.padding = '10px 10px';
