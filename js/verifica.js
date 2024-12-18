62ef84068618530caa95ee254e15a2783bc1aaf3
inforf <30818563+inforf@users.noreply.github.com>
2024-12-18 20:13:40 -0300

    Update verifica.js

--- js/verifica.js
+++ js/verifica.js
@@ -13,9 +13,7 @@ function verificarElementos() {
         // Ajusta o posicionamento de #abrefechacaixa para evitar sobreposição
         const avisoRect = avisoIfoodInteg?.getBoundingClientRect();
         if (avisoRect) {
-            // abrefechacaixa.style.position = "absolute";
-            abrefechacaixa.style.top = ${avisoRect.bottom + 160}px; // Ajusta 10px abaixo de #avisoIfoodInteg
-            //abrefechacaixa.style.left = ${avisoRect.left}px; // Alinha à esquerda de #avisoIfoodInteg
+            abrefechacaixa.style.marginTop = "60px";
         }
     }
 }
