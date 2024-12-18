6361e7b9cf428efc2a5601500502cc2945115d41
inforf <30818563+inforf@users.noreply.github.com>
2024-12-18 19:12:08 -0300

    Update verifica.js

--- js/verifica.js
+++ js/verifica.js
@@ -15,7 +15,7 @@ function verificarElementos() {
         if (avisoRect) {
             abrefechacaixa.style.position = "absolute";
             abrefechacaixa.style.top = `${avisoRect.bottom + 10}px`; // Ajusta 10px abaixo de #avisoIfoodInteg
-            abrefechacaixa.style.left = `${avisoRect.left}px`; // Alinha à esquerda de #avisoIfoodInteg
+            //abrefechacaixa.style.left = `${avisoRect.left}px`; // Alinha à esquerda de #avisoIfoodInteg
         }
     }
 }
