0e4336294c989c6cc8510873cdd972fcb6855d09
inforf <30818563+inforf@users.noreply.github.com>
2024-12-18 20:36:29 -0300

    Update verifica.js

--- js/verifica.js
+++ js/verifica.js
@@ -1,4 +1,3 @@
-// Intervalo de verificação em milissegundos
 const CHECK_INTERVAL = 1000; // 1 segundo
 
 function verificarElementos() {
@@ -7,10 +6,8 @@ function verificarElementos() {
     const abrefechacaixa = document.querySelector("#abrefechacaixa");
 
     if (!divListaIntegracoes && avisoIfoodInteg) {
-        // Remove o elemento #avisoIfoodInteg se #divListaIntegracoes não existir
         avisoIfoodInteg.remove();
     } else if (divListaIntegracoes && abrefechacaixa) {
-        // Ajusta o posicionamento de #abrefechacaixa para evitar sobreposição
         const avisoRect = avisoIfoodInteg?.getBoundingClientRect();
         if (avisoRect) {
             abrefechacaixa.style.marginTop = "60px";
@@ -18,5 +15,4 @@ function verificarElementos() {
     }
 }
 
-// Configura a verificação periódica
 setInterval(verificarElementos, CHECK_INTERVAL);
